60bac2ef5a7ace4db1670e050ac51a511f084da6
Dirk Uys <dirkcuys@gmail.com>
2013-02-19 13:00:32 +0200

    move work of sending email to separate function

---
+++ signup/emails.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.template.loader import render_to_string
+
+import mailgun
+
+def send_welcome_email(email):
+    subject = render_to_string('signup/emails/signup-confirmation-subject.txt', {}).strip()
+    text_body = render_to_string('signup/emails/signup-confirmation.txt', {}).strip()
+    html_body = render_to_string('signup/emails/signup-confirmation.html', {}).strip()
+    mailgun.send_email(settings.DEFAULT_FROM_EMAIL, subject, text_body, html_body)
--- signup/tasks.py
+++ signup/tasks.py
@@ -1,12 +1,8 @@
 from celery import task
-from django.conf import settings
-from django.template.loader import render_to_string
 
-import mailgun
+import emails
 
 @task
 def send_welcome_email(email):
-    subject = render_to_string('signup/emails/signup-confirmation-subject.txt', {}).strip()
-    text_body = render_to_string('signup/emails/signup-confirmation.txt', {}).strip()
-    html_body = render_to_string('signup/emails/signup-confirmation.html', {}).strip()
-    mailgun.send_email(settings.DEFAULT_FROM_EMAIL, subject, text_body, html_body)
+    """ Celery task to send email asynchronously """
+    emails.send_welcome_email(email)
