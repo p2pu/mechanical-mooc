97a25de5bfcf570930efc7f64cd2804b6035ab68
Dirk Uys <dirkcuys@gmail.com>
2013-04-16 10:59:24 +0200

    Explicit syncing with mailgun lists

--- groups/models.py
+++ groups/models.py
@@ -16,8 +16,6 @@ def group_id2uri( group_id ):
 def create_group( address, description ):
     group_db = db.Group(address=address, description=description)
     group_db.save()
-    mailgun_api.create_list(address, description=description, access_level='members')
-    mailgun_api.add_list_member(address, settings.DEFAULT_FROM_EMAIL)
     return get_group(group_id2uri(group_db.id))
 
 
@@ -44,14 +42,43 @@ def get_groups():
 def add_group_member( group_uri, member_email ):
     group_id = group_uri2id(group_uri)
     group_db = db.Group.objects.get(id=group_id)
-    member = db.GroupMember(email=member_email, group=group_db)
-    member.save()
-    mailgun_api.add_list_member(group_db.address, member.email)
+    if not group_db.members.filter(email=member_email).exists():
+        member = db.GroupMember(email=member_email, group=group_db)
+        member.save()
 
 
 def remove_group_member( group_uri, member_email ):
-    raise Exception('Not implemented')
     group_id = group_uri2id(group_uri)
     group_db = db.Group.objects.get(id=group_id)
     group_db.members.get(email=member_email).delete()
-    
+
+
+def sync_group_with_mailgun( group_uri ):
+    group = get_group(group_uri)
+
+    # check if group exists on mailgun, if not - create
+    mg_group = mailgun_api.get_list(group['address'])
+    if not mg_group:
+        mailgun_api.create_list(group['address'], description=group['description'], access_level='members' )
+
+    # get list of members
+    mg_members = mailgun_api.get_list_members(group['address'])
+    mg_member_set = [m['address'] for m in mg_members]
+
+    # for every member locally not in mailgun, add them
+    for member in group['members']:
+        if member not in mg_member_set:
+            # add member to mailgun
+            mailgun_api.add_list_member(group['address'], member)
+
+    # for every member on mailgun
+    for member in mg_members:
+        # if unsubscribed on mailgun, remove locally
+        if member['subscribed'] == False and member['address'] in group['members']:
+            # remove member from local group
+            remove_group_member(group_uri, member['address'])
+
+        # if on mailgun, but not locally, remove from mailgun
+        if member['subscribed'] == True and member['address'] not in group['members']:
+            mailgun_api.remove_list_member(group['address'], member['address'])
+
