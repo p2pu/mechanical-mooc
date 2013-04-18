b4026f66638b0116b22d2e77af1bda869c0d5d1f
Dirk Uys <dirkcuys@gmail.com>
2013-04-18 11:46:04 +0200

    Add management command to send scheduled emails

---
+++ mail/management/commands/send_scheduled_mail.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from mail import models as mail_api
+from mail.email import send_email_to_groups
+
+from datetime import datetime
+
+class Command(BaseCommand):
+    #args = '<data url>'
+    help = 'Send all scheduled email with a date in the past'
+
+    def handle(self, *args, **options):
+        emails = mail_api.get_emails()
+        for email in emails:
+            date_scheduled = email.get('date_scheduled')
+            date_sent = email.get('date_sent')
+            if not date_sent and date_scheduled and date_scheduled < datetime.utcnow():
+                print(u'sending email {0}'.format(email['subject']))
+                send_email_to_groups(email['uri'])
