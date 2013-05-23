c7ef5d2c049beba4bd1b12ec2e62a61446746a8a
Dirk Uys <dirkcuys@gmail.com>
2013-05-23 14:53:09 +0200

    Return http 200 for webhooks

--- unsubscribe/views.py
+++ unsubscribe/views.py
@@ -1,8 +1,10 @@
 from django import http
-from mailgun import utils
+from django.views.decorators.csrf import csrf_exempt
 
+from mailgun import utils
 import models as unsubscribe_model
 
+@csrf_exempt
 def unsubscribe_webhook(request):
     verified = utils.verify_webhook(
         request.POST.get('token'), 
@@ -15,8 +17,13 @@ def unsubscribe_webhook(request):
 
     address = request.POST.get('recipient')
 
-    if request.POST.get('mailing-list'):
-        unsubscribe_model.unsubscribe_from_sequence(address)
-    else:
-        unsubscribe_model.unsubscribe_completely(address)
+    try:
+        if request.POST.get('mailing-list'):
+            unsubscribe_model.unsubscribe_from_sequence(address)
+        else:
+            unsubscribe_model.unsubscribe_user(address)
+    except:
+        raise
+
+    return http.HttpResponse('')
 
