e1a11833e10d8b31c64f43db59bc169f91f4d66d
Dirk Uys <dirkcuys@gmail.com>
2013-05-24 13:34:40 +0200

    Use sequence model interface to get mailing list name and campaign name

--- mail/email.py
+++ mail/email.py
@@ -4,6 +4,7 @@ from django.template.loader import render_to_string
 from mail import models as mail_api
 from groups import models as group_api
 from mailgun import api as mailgun_api
+from sequence import models as sequence_api
 
 def send_email( email_uri ):
     """ Send the email to the intended target audience """
@@ -12,7 +13,7 @@ def send_email( email_uri ):
     if email['audience'] == 'groups':
         to_address = ','.join([g['address'] for g in group_api.get_groups(email['sequence'])])
     elif email['audience'] == 'individuals':
-        to_address = 'sequence-{0}-all@{1}'.format(email['sequence'], settings.EMAIL_DOMAIN)
+        to_address = sequence_api.sequence_list_name(email['sequence'])
 
     text_body = render_to_string('mail/email.txt', {'email': email})
     html_body = render_to_string('mail/email.html', {'email': email})
@@ -24,6 +25,6 @@ def send_email( email_uri ):
         text_body,
         html_body,
         email['tags'].split(','),
-        'sequence-{0}-campaign'.format(email['sequence'])
+        sequence_api.sequence_campaign(email['sequence'])
     )
     mail_api.mark_sent(email_uri)
