3696e1c0e77134365220ad0d2cfa9bae711932a3
Dirk Uys <dirkcuys@gmail.com>
2013-06-14 15:26:23 +0200

    New grouping strategy

--- grouping/models.py
+++ grouping/models.py
@@ -8,19 +8,37 @@ import math
 import datetime
 import pytz
 
+
+def shuffle(rac):
+    shuffled = []
+    while len(rac):
+        ridx = random.randint(0, len(rac)-1)
+        shuffled += [rac[ridx]]
+        del rac[ridx]
+    return shuffled
+
 def prepare_groups(max_group_size=40):
     """ Calculate grouping """
 
     # get all signups
     signups = signup_model.get_signups()
+    tz_grouping = {}
 
     for user_signup in signups:
-        #NOTE: this is an implicit requirement on the data needed in questions - not good!
         timezone = user_signup['questions']['timezone']
-        tz_offset = datetime.datetime.now(pytz.timezone(timezone)).strftime('%z')
+        tz_offset = int(datetime.datetime.now(pytz.timezone(timezone)).strftime('%z'))
         user_signup['tz_offset'] = tz_offset
+        if tz_offset in tz_grouping:
+            tz_grouping[tz_offset].append(user_signup)
+        else:
+            tz_grouping[tz_offset] = [user_signup]
 
-    tz_sorted_signups = sorted(signups, key=lambda x: x['tz_offset'])
+    for timezone, group in tz_grouping.items():
+        tz_grouping[timezone] = shuffle(group)
+
+    merge = lambda x,y: x + y
+    tz_sorted_signups = reduce(merge, [ tz_grouping[timezone] for timezone in sorted(tz_grouping.keys())])
+
     groups = [ tz_sorted_signups[i:min(i+max_group_size, len(tz_sorted_signups))] for i in range(0,len(tz_sorted_signups), max_group_size)]
     return groups
 
