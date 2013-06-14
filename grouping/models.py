ee8cf417ffd2f4dea0c6228634ff86de92958ad3
Dirk Uys <dirkcuys@gmail.com>
2013-06-14 09:09:06 +0200

    Get timezone offset for users

--- grouping/models.py
+++ grouping/models.py
@@ -1,10 +1,14 @@
+from django.conf import settings
+
 from groups import models as group_model
 from signup import models as signup_model
 
 import random
 import math
+import datetime
+import pytz
 
-def prepare_groups(max_group_size=20):
+def prepare_groups(max_group_size=40):
     """ Calculate grouping """
 
     # get all signups
@@ -13,9 +17,7 @@ def prepare_groups(max_group_size=20):
     for user_signup in signups:
         #NOTE: this is an implicit requirement on the data needed in questions - not good!
         timezone = user_signup['questions']['timezone']
-        tz_offset = int(timezone[:timezone.index(' ')].replace(':',''))
-        if tz_offset == -1200:
-            tz_offset = 1200
+        tz_offset = datetime.datetime.now(pytz.timezone(timezone)).strftime('%z')
         user_signup['tz_offset'] = tz_offset
 
     tz_sorted_signups = sorted(signups, key=lambda x: x['tz_offset'])
@@ -26,7 +28,7 @@ def prepare_groups(max_group_size=20):
 def create_groups(groups, name_prefix="Group"):
     """ Create the groups in the backend """
     for i, group_data in enumerate(groups):
-        group_address = "{0}-{1}@data.p2pu.org".format(name_prefix.lower().replace(' ','-'), i+1)
+        group_address = "{0}-{1}@{2}".format(name_prefix.lower().replace(' ','-'), i+1, settings.EMAIL_DOMAIN)
         group_name = "{0} {1}: {2} to {3}".format(name_prefix, i+1, group_data[0]['tz_offset'], group_data[-1]['tz_offset'])
         print(group_name)
         group = group_model.create_group(group_address, group_name)
