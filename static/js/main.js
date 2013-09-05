c9546a6172319b2d9db2e40b544a7c6ab33fa74d
ercchy <erika@serox.si>
2013-09-05 19:07:06 +0200

    pushed some logos around

--- static/js/main.js
+++ static/js/main.js
@@ -13,93 +13,92 @@ var MMOOC = window.MMOOC || {};
                 source: '.nav-collapse.collapse'
             });
         });
-    }
+    },
+        timezone_detection_handler = function () {
+            var timezone = jstz.determine();
+            $("#timezone").val(timezone.name());
+        },
 
-    , timezone_detection_handler = function () {
-        var timezone = jstz.determine();
-        $("#timezone").val(timezone.name());
-    }
-
-    , country_select_handler = function () {
-        $("#id_country_select").prop("selectedIndex", -1);
-        $("#id_country_select").change(function () {
-            if ($('#id_country_select').val() == 'United States') {
-                $('#id_state_input').show();
-            } else {
-                $('#id_state_input').hide();
-            }
-            $("#id_state_select").prop("selectedIndex", -1);
-        });
-    }
-
-    , email_validation_handler = function () {
-        $("#emailForm").validate({
-            debug: true,
-            rules: {
-                email: {
-                    required: true,
-                    email: true
+        country_select_handler = function () {
+            $("#id_country_select").prop("selectedIndex", -1);
+            $("#id_country_select").change(function () {
+                if ($('#id_country_select').val() == 'United States') {
+                    $('#id_state_input').show();
+                } else {
+                    $('#id_state_input').hide();
                 }
-            },
-            errorClass: "error",
-            highlight: function (element, errorClass) {
-                $("#emailForm .control-group").addClass(errorClass);
-            },
-            unhighlight: function (element, errorClass, validClass) {
-                $("#emailForm .control-group").removeClass(errorClass);
-            },
-            errorPlacement: function (error, element) {
-                $("#emailForm .control-group").prepend(error);
-            },
-            submitHandler: function (form) {
-                $("#hiddenEmailField").val($("#email").val());
-                $("#step1").hide("slide", { direction: "left" }, 500, function () {
-                    $("#step2").show("slide", { direction: "right" }, 500);
-                });
-            }
-        });
-    }
+                $("#id_state_select").prop("selectedIndex", -1);
+            });
+        },
 
-    ,info_form_validation_handler = function () {
-        $("#infoForm").validate({
-            debug: true,
-            rules: {
-                username: "required",
-                educationrole: "required",
-                country: "required",
-                us_state: {
-                    required: {
-                        depends: function () {
-                            return $('#id_country_select').val() == 'United States';
-                        }
+        email_validation_handler = function () {
+            $("#emailForm").validate({
+                debug: true,
+                rules: {
+                    email: {
+                        required: true,
+                        email: true
                     }
                 },
-                timezone: "required",
-            },
-            errorClass: "error",
-            highlight: function (element, errorClass) {
-                $(element).parents(".control-group").addClass(errorClass);
-            },
-            unhighlight: function (element, errorClass, validClass) {
-                $(element).parents(".control-group").removeClass(errorClass);
-            },
-            errorPlacement: function () {
-            },
-            submitHandler: function (form) {
-                form.submit();
-            }
-        });
-    }
+                errorClass: "error",
+                highlight: function (element, errorClass) {
+                    $("#emailForm .control-group").addClass(errorClass);
+                },
+                unhighlight: function (element, errorClass, validClass) {
+                    $("#emailForm .control-group").removeClass(errorClass);
+                },
+                errorPlacement: function (error, element) {
+                    $("#emailForm .control-group").prepend(error);
+                },
+                submitHandler: function (form) {
+                    $("#hiddenEmailField").val($("#email").val());
+                    $("#step1").hide("slide", { direction: "left" }, 500, function () {
+                        $("#step2").show("slide", { direction: "right" }, 500);
+                    });
+                }
+            });
+        },
 
-    , signup = function () {
+        info_form_validation_handler = function () {
+            $("#infoForm").validate({
+                debug: true,
+                rules: {
+                    username: "required",
+                    educationrole: "required",
+                    country: "required",
+                    us_state: {
+                        required: {
+                            depends: function () {
+                                return $('#id_country_select').val() == 'United States';
+                            }
+                        }
+                    },
+                    timezone: "required",
+                },
+                errorClass: "error",
+                highlight: function (element, errorClass) {
+                    $(element).parents(".control-group").addClass(errorClass);
+                },
+                unhighlight: function (element, errorClass, validClass) {
+                    $(element).parents(".control-group").removeClass(errorClass);
+                },
+                errorPlacement: function () {
+                },
+                submitHandler: function (form) {
+                    form.submit();
+                }
+            });
+        },
 
-        $(function () {
-            email_validation_handler();
-            timezone_detection_handler();
-            country_select_handler();
-            info_form_validation_handler();
-        });
-    }
+        signup = function () {
+
+            $(function () {
+                email_validation_handler();
+                timezone_detection_handler();
+                country_select_handler();
+                info_form_validation_handler();
+            });
+        };
 
     MMOOC.Splash = {};
     MMOOC.Splash.init = init;
