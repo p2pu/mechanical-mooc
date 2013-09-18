cb5ccfe3b696c50bb4c30043d4431730992e3bfd
Dirk Uys <dirkcuys@gmail.com>
2013-09-18 11:38:20 -0300

    Update grouping test to use new interface for random data

--- grouping/tests.py
+++ grouping/tests.py
@@ -10,10 +10,10 @@ import random
 
 class SimpleTest(TestCase):
     def test_prepare_groups(self):
-        for signup in randata.random_signup(1000):
+        for signup in randata.random_data(1000):
             signup_model.create_or_update_signup(**signup)
 
-        groups = models.prepare_groups()
+        groups = models.prepare_groups(1)
 
         for i in range(100):
             random_group = random.choice(groups)
@@ -23,10 +23,10 @@ class SimpleTest(TestCase):
 
 
     def test_every_man_is_an_island(self):
-        for signup in randata.random_signup(1000):
+        for signup in randata.random_data(1000):
             signup_model.create_or_update_signup(**signup)
 
-        groups = models.prepare_groups(max_group_size=1)
+        groups = models.prepare_groups(1, max_group_size=1)
 
         for i in range(100):
             random_group = random.choice(groups)
@@ -35,17 +35,17 @@ class SimpleTest(TestCase):
             self.assertEqual(len(random_group['members']), 1)
 
     def test_here_comes_everyone(self):
-        for signup in randata.random_signup(1000):
+        for signup in randata.random_data(1000):
             signup_model.create_or_update_signup(**signup)
-        groups = models.prepare_groups(max_group_size=1001)
+        groups = models.prepare_groups(1, max_group_size=1001)
         self.assertEqual(len(groups), 0)
 
 
     def test_group_creation(self):
-        for signup in randata.random_signup(1000):
+        for signup in randata.random_data(1000):
             signup_model.create_or_update_signup(**signup)
 
-        group_data = models.prepare_groups()
+        group_data = models.prepare_groups(1)
         models.create_groups(group_data, 'g-')
         groups = group_model.get_groups()
 
