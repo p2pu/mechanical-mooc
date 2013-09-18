5bc3ebc88fecf64f15862fb6115ab6fb768e6d2f
Dirk Uys <dirkcuys@gmail.com>
2013-09-18 11:36:39 -0300

    Fix wrong URL in gallery/urls.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
     url(r'^$', 'gallery.views.gallery', name='gallery_gallery'),
     url(r'^save_bio/$', 'gallery.views.save_bio', name='gallery_save_bio'),
     url(r'^success', 'gallery.views.avatar_success', name='gallery_avatar_success'),
-    url(r'^confirm_updates/(?P<confirmation_code>.*)/$', 'gallery.views.confirm_updates', name='gallery_confirm_updates'),
+    url(r'^confirm_updates/(?P<confirmation_code>.*)/$', 'gallery.views.confirm_updates', name='gallery_confirm_update'),
 )
