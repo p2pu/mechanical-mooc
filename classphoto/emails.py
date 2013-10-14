f7383b5bbd50a0b680b91522a59db94c7774ad53
Dirk Uys <dirkcuys@gmail.com>
2013-10-14 16:48:42 -0300

    Send classphoto links to users in sequence in batches of 1000

--- classphoto/emails.py
+++ classphoto/emails.py
@@ -4,6 +4,7 @@ from django.utils import simplejson as json
 
 import mailgun
 from signup import models as signup_api
+from sequence import models as sequence_api
 
 
 def send_user_link( email, key ):
@@ -20,12 +21,7 @@ def send_user_link( email, key ):
 
 
 def send_user_link_to_whole_sequence( sequence ):
-    signups = signup_api.get_signups(sequence)
-    emails = [ signup['email'] for signup in signups ]
-    recipient_variables = {
-        signup['email']: {'key': signup['key']} for signup in signups
-    }
-
+    all_signups = signup_api.get_signups(sequence)
     context = {
         'mooc_title': settings.MOOC_TITLE,
         'mooc_domain': settings.MOOC_DOMAIN,
@@ -36,15 +32,22 @@ def send_user_link_to_whole_sequence( sequence ):
     text_body = render_to_string('classphoto/emails/sequence_links.txt', context).strip()
     html_body = render_to_string('classphoto/emails/sequence_links.html', context).strip()
 
-    #TODO: send in batches of 1000 emails
-    #TODO: include campaign id for sequence 
-    mailgun.api.send_mass_email(
-        emails,
-        settings.DEFAULT_FROM_EMAIL,
-        subject,
-        text_body,
-        html_body,
-        tags=['classphotolink'],
-        recipient_variables=json.dumps(recipient_variables)
-    )
+    for i in range(0, len(all_signups), 1000):
+        signups = all_signups[i:i+1000]
+        emails = [ signup['email'] for signup in signups ]
+        recipient_variables = {
+            signup['email']: {'key': signup['key']} for signup in signups
+        }
+
+        #TODO: include campaign id for sequence 
+        mailgun.api.send_mass_email(
+            emails,
+            settings.DEFAULT_FROM_EMAIL,
+            subject,
+            text_body,
+            html_body,
+            tags=['classphotolink'],
+            campaign_id=sequence_api.sequence_campaign(sequence),
+            recipient_variables=json.dumps(recipient_variables)
+        )
 
