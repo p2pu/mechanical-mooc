d12e6d8b45ade5025a81d1e5f04dc91613bc8623
Dirk Uys <dirkcuys@gmail.com>
2013-10-25 14:03:59 -0300

    Add method to classphoto to get bios by list of email addresses

--- classphoto/models.py
+++ classphoto/models.py
@@ -39,6 +39,15 @@ def save_bio( email, sequence, name, bio, avatar, twitter=None ):
     return _bio2dict(bio_db)
 
 
+def has_bio( email, sequence ):
+    bios_db = db.UserBio.objects.filter(
+        email=email,
+        sequence=sequence,
+        date_deleted__isnull=True
+    )
+    return bios_db.exists()
+
+
 def get_bio( email ):
     bios_db = db.UserBio.objects.filter(
         email=email,
@@ -54,4 +63,12 @@ def get_bios( sequence, limit=100 ):
     )
     if limit > 0:
         bios_db = bios_db[:limit]
+    return [ _bio2dict(bio) for bio in bios_db ]
+
+
+def get_bios_by_email( sequence, emails ):
+    bios_db = db.UserBio.objects.filter(
+        sequence=sequence,
+        email__in=emails
+    )
     return [ _bio2dict(bio) for bio in bios_db ]
