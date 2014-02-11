345e6b1f3a32baddd7947e70bce62d1c23a1753d
Dirk Uys <dirkcuys@gmail.com>
2014-02-11 18:36:05 -0300

    Update code to get emails and process them for storage

--- data/emails.py
+++ data/emails.py
@@ -1,24 +1,43 @@
 import imaplib
-from email.parser import HeaderParser
+from email import message_from_string
 from django.conf import settings
 
 def get_emails():
+    """ Download all the raw emails from the IMAP server and return a list of email.message.Message """
     conn = imaplib.IMAP4('imap.mailgun.org')
-    conn.login('the-machine@mechanicalmooc.org', settings.MAILBOX_PASSWORD)
-    conn.select()
-    conn.search(None, 'ALL') # returns a nice list of messages...
+    conn.login(settings.DEFAULT_FROM_EMAIL, settings.MAILBOX_PASSWORD)
+    conn.select() # select inbox
+    typ, message_ids = conn.uid('search', None, 'ALL') # get a list of all messages
+    msg_count = len(message_ids[0].split())
 
-    data = conn.fetch('1:*', '(BODY[HEADER])')
+    messages = []
+    for i, uid in enumerate(message_ids[0].split()):
+        print('Downloading message {0} of {1}'.format(i, msg_count))
+        typ, data = conn.uid('fetch', uid, '(BODY[])')
+        msg = message_from_string(data[0][1])
+        messages += [msg]
+    return messages
 
-    print(len(data[1]))
 
+def process_messages(email_list):
+    """ takes a list of messages and convert them to a format we can shove into CSV """
     message_fields = ['From', 'To', 'Subject', 'Date']
     messages = []
-    for i in range(0, len(data[1]), 2):
-        print(i)
-        header_data = data[1][i][1]
-        #print(header_data)
-        parser = HeaderParser()
-        msg = parser.parsestr(header_data)
-        messages += [ { key: msg[key] for key in message_fields } ]
+    for msg in email_list:
+        message = { key: msg[key] for key in message_fields }
+        body = ''
+        if msg.is_multipart():
+            # Make a best guess at the body of the message
+            # 1. Use text/plain
+            # 2. Use text/html
+            for part in msg.walk():
+                if body == '' and part.get_content_type() == 'text/html':
+                    body = part.get_payload()
+                if part.get_content_type() == 'text/plain':
+                    body = part.get_payload()
+        else:
+            body = msg.get_payload()
+        message['body'] = body
+        messages += [message]
     return messages
+
